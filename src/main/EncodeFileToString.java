2223912367b0b04fdbbeffde77edea6b8451f62d
Hanbe.T <hanbet@koba-masa.com>
2017-12-09 22:31:10 +0900

    ファイルエンコードツール [分類]   追加
    [機能名]
      ファイルエンコード機能
    
    [概要]
      指定されたファイルを文字列にエンコードする。

---
+++ src/main/EncodeFileToString.java
@@ -0,0 +1,17 @@
+package main;
+
+public class EncodeFileToString {
+
+    public static void main(String[] args) {
+
+        // ファイル一覧取得
+
+
+
+    }
+
+    private void mainLogic(String filePath) {
+
+    }
+
+}
---
+++ src/sg/ComConst.java
@@ -0,0 +1,11 @@
+package sg;
+
+public class ComConst {
+    /** 取得元ディレクトリパス */
+    public static final String ORG_DIR_PATH = "input";
+
+    /** 生成先ディレクトリパス */
+    public static final String SRC_DIR_PATH = "output";
+
+
+}
